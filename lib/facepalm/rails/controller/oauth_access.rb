fdbc99768e1e5279b988eebca3d55be81141c373
Volodymyr Stelmakh <vk.stelmakh@gmail.com>
2015-04-10 15:12:04 +0300

    Escape fb_return_to url

--- lib/facepalm/rails/controller/oauth_access.rb
+++ lib/facepalm/rails/controller/oauth_access.rb
@@ -77,7 +77,7 @@ module Facepalm
             redirect_to = facepalm.oauth_client.url_for_oauth_code(
                             :permissions => permissions,
                             :callback => facepalm_endpoint_url(
-                              :fb_return_to => facepalm_auth_return_code
+                              :fb_return_to => ::Rack::Utils.escape(facepalm_auth_return_code)
                             )
             )
             ::Rails.logger.fatal("User #{current_facebook_user} is not authenticated\nRedirecting to #{redirect_to}")
